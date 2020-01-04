17209e4f6ce726340359a820e012dbb370d66f2d
Matvey <tod13yung@yandex.ru>
2020-01-04 23:53:43 +0400

    Started a project with network

---
+++ Assets/Scripts/Network/NetworkCustomGUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkCustomGUI : MonoBehaviour {
+
+    public NetworkManager network;
+    public GUISkin gskin;
+
+    void Start()
+    {
+        network = GetComponent<NetworkManager>();
+    }
+
+	void OnGUI()
+    {
+        GUI.skin = gskin;
+        GUI.Label(new Rect(Screen.width * 0.15f, Screen.height * 0.01f, Screen.width * 0.5f, Screen.height * 0.09f), "Please, choose an option");
+
+    }
+}
